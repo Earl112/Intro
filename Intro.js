31f5e392cd470270806a448e85005bc00986350f
Eddy Ekot <edidiongekot18@gmail.com>
2026-01-17 13:41:05 +0100

    initial commit: First nodes backend log project

---
+++ Intro.js
@@ -0,0 +1,6 @@
+const Name = "Eddy";
+   let age = 25;
+   const Favourite_Programing_Language = "Java";
+console.log("Name: " + Name);
+console.log("Age: " + age);
+console.log("Favorite Programming Language: " + Favourite_Programing_Language);
